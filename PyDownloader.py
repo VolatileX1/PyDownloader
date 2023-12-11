f7e6d3c0e76706455052924eacee3ef789f02a73
VolatileX1 <128452078+VolatileX1@users.noreply.github.com>
2023-12-11 13:14:15 +0530

    Update PyDownloader.py
    - Added progress bar using the "tqdm" library

--- PyDownloader.py
+++ PyDownloader.py
@@ -1,21 +1,30 @@
 import requests
 import os
+from tqdm import tqdm
 
-def download_file(url, destination):
-    """Download a file from the given URL and save it to the specified destination."""
+def download_file_with_progress(url, destination):
+    """Download a file from the given URL and save it to the specified destination with a progress bar."""
     try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an exception for bad responses (4xx and 5xx)
-        
+        response = requests.get(url, stream=True)
+        response.raise_for_status()
+
+        total_size = int(response.headers.get('content-length', 0))
+        block_size = 1024  # 1 KB
+        progress_bar = tqdm(total=total_size, unit='B', unit_scale=True, desc='Downloading', ncols=80)
+
         with open(destination, 'wb') as file:
-            file.write(response.content)
-        
-        print(f"Download successful. File saved at: {destination}")
+            for data in response.iter_content(block_size):
+                progress_bar.update(len(data))
+                file.write(data)
+
+        progress_bar.close()
+
+        print(f"\nDownload successful. File saved at: {destination}")
     except requests.exceptions.RequestException as e:
         print(f"Error downloading file: {e}")
 
 def main():
-    print("=== PyDownloader ===")
+    print("=== PyDownloader with Progress Bar ===")
     url = input("Enter the URL of the file to download: ")
     
     # Extracting the filename from the URL
@@ -28,7 +37,7 @@ def main():
     elif os.path.isdir(destination):
         destination = os.path.join(destination, filename)
     
-    download_file(url, destination)
+    download_file_with_progress(url, destination)
 
 if __name__ == "__main__":
     main()
