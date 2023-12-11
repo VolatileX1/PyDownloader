73c0e24d7a41f0e66184c09b7c13210fd0aceaa5
VolatileX1 <128452078+VolatileX1@users.noreply.github.com>
2023-12-11 13:17:53 +0530

    Update PyDownloader.py
    - Added multiple download support
    - Added File validation
    
    The script now allows users to input multiple URLs separated by spaces. It validates the file type before downloading each file and provides a warning if the file type is not allowed.

--- PyDownloader.py
+++ PyDownloader.py
@@ -15,7 +15,7 @@ def download_file_with_progress(url, destination):
 
         total_size = int(response.headers.get('content-length', 0)) + os.path.getsize(destination)
         block_size = 1024  # 1 KB
-        progress_bar = tqdm(total=total_size, initial=os.path.getsize(destination), unit='B', unit_scale=True, desc='Downloading', ncols=80)
+        progress_bar = tqdm(total=total_size, initial=os.path.getsize(destination), unit='B', unit_scale=True, desc=f'Downloading {os.path.basename(destination)}', ncols=80)
 
         with open(destination, 'ab') as file:
             for data in response.iter_content(block_size):
@@ -28,21 +28,37 @@ def download_file_with_progress(url, destination):
     except requests.exceptions.RequestException as e:
         print(f"Error downloading file: {e}")
 
-def main():
-    print("=== PyDownloader with Progress Bar and Resumable Download ===")
-    url = input("Enter the URL of the file to download: ")
-    
-    # Extracting the filename from the URL
+def download_multiple_files(urls, destination_folder="."):
+    """Download multiple files sequentially."""
+    for url in urls:
+        filename = url.split("/")[-1]
+        destination = os.path.join(destination_folder, filename)
+        download_file_with_progress(url, destination)
+
+def validate_file_type(url, allowed_file_types):
+    """Check if the file type is allowed for download."""
     filename = url.split("/")[-1]
+    file_extension = os.path.splitext(filename)[1][1:]  # Remove the leading dot
+
+    if not file_extension.lower() in allowed_file_types:
+        print(f"Warning: File type '{file_extension}' not allowed for download from {url}.")
+        return False
+    return True
+
+def main():
+    print("=== PyDownloader with Progress Bar, Resumable Download, Multiple File Downloads, and File Type Validation ===")
     
-    destination = input(f"Enter the destination path to save '{filename}' (or press Enter to save in the current directory): ")
-    
-    if not destination:
-        destination = filename
-    elif os.path.isdir(destination):
-        destination = os.path.join(destination, filename)
-    
-    download_file_with_progress(url, destination)
+    # Get multiple URLs from the user
+    urls = input("Enter multiple URLs separated by spaces: ").split()
+
+    # Choose destination folder
+    destination_folder = input("Enter the destination folder (or press Enter to save in the current directory): ") or "."
+
+    # Validate and download files
+    allowed_file_types = ["jpg", "jpeg", "png", "gif", "txt", "pdf"]  # Example allowed file types
+    for url in urls:
+        if validate_file_type(url, allowed_file_types):
+            download_multiple_files([url], destination_folder)
 
 if __name__ == "__main__":
     main()
